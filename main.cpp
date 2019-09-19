267f92f5d2b5ecde2c92092b16c2ba809a6c7ba5
mabur <magnus.burenius@volumental.com>
2019-09-19 13:39:40 +0200

    Add focus depth

--- main.cpp
+++ main.cpp
@@ -26,7 +26,10 @@ const size_t IMAGE_HEIGHT = 1000;
 const double RADIUS = min(IMAGE_HEIGHT, IMAGE_WIDTH) * 0.4;
 const size_t NUM_STEPS = 8000;
 const double STEP_SIZE = 0.09;
-const size_t NUM_SAMPLES_PER_LINE = 30;
+const size_t NUM_SAMPLES_PER_LINE = 100;
+const double BRIGHTNESS = 1000.0;
+const auto BLUR = 0.02;
+const auto BLUR_EXPONENT = 2.0;
 
 auto g = bind(      normal_distribution<double>(), mt19937(0));
 auto u = bind(uniform_real_distribution<double>(), mt19937(0));
@@ -55,11 +58,20 @@ vector<double> random_walk_on_sphere()
         for (size_t s = 0; s < NUM_SAMPLES_PER_LINE; ++s)
         {
             const auto d = u();
-            const auto point = (1.0 - d) * p0 + d * p1;
-            // point = unfocus(point);
-            const auto x = static_cast<size_t>(point[0] * RADIUS) + IMAGE_WIDTH  / 2;
-            const auto y = static_cast<size_t>(point[1] * RADIUS) + IMAGE_HEIGHT / 2;
-            image[y * IMAGE_WIDTH + x] = 1;
+            auto point = vec{(1.0 - d) * p0 + d * p1};
+
+            const auto dz = 1.0 - point[2];
+            point += BLUR * pow(dz, BLUR_EXPONENT) * random_direction();
+
+            const auto x = point[0] * RADIUS + IMAGE_WIDTH  * 0.5;
+            const auto y = point[1] * RADIUS + IMAGE_HEIGHT * 0.5;
+
+            if (0.0 <= x && x < IMAGE_WIDTH && 0.0 <= y and y < IMAGE_HEIGHT)
+            {
+                const auto xi = static_cast<size_t>(x);
+                const auto yi = static_cast<size_t>(y);
+                ++image[yi * IMAGE_WIDTH + xi];
+            }
         }
     }
     const auto maximum = *max_element(begin(image), end(image));
@@ -71,15 +83,6 @@ vector<double> random_walk_on_sphere()
     return image;
 }
 
-//vec unfocus(vec v)
-//{
-//    const auto BLUR = 1.0;
-//    const auto z = v[2];
-//    const auto dir = random_direction();
-//    const auto offset = scale(BLUR * z, dir);
-//    return add(v, offset);
-//}
-
 vector<double> write_image(const vector<double>& image, const char* filename)
 {
     ofstream file(filename);
@@ -88,7 +91,7 @@ vector<double> write_image(const vector<double>& image, const char* filename)
     {
         auto NUM_COLOR_CHANNELS = 3;
         for (auto c = 0; c < NUM_COLOR_CHANNELS; ++c)
-            file << min(int(pixel * 255.0), 255) << " ";
+            file << min(int(pixel * BRIGHTNESS), 255) << " ";
     }
     file.close();
 }
