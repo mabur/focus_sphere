ab70548b4b48bc7b081f1c279c356dd5f1aae786
mabur <magnus.burenius@volumental.com>
2019-09-19 11:09:39 +0200

    Random walk on sphere

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@
 
 using namespace std;
 
-const size_t IMAGE_WIDTH  = 500;
-const size_t IMAGE_HEIGHT = 500;
-const double RADIUS = 200;
+const size_t IMAGE_WIDTH  = 360;
+const size_t IMAGE_HEIGHT = 360;
+const double RADIUS = min(IMAGE_HEIGHT, IMAGE_WIDTH) * 0.4;
 
-auto g = bind(      normal_distribution<double>(), mt19937());
-auto u = bind(uniform_real_distribution<double>(), mt19937());
+auto g = bind(      normal_distribution<double>(), mt19937(0));
+auto u = bind(uniform_real_distribution<double>(), mt19937(0));
 // Definition of vector type:
 using vec = valarray<double>;
 double dot(vec a, vec b)
@@ -28,22 +28,14 @@ vec normalized(vec a)
 {
     return a / norm(a);
 }
-vec add(vec a, vec b)
-{
-    return {a[0] + b[0], a[1] + b[1], a[2] + b[2]};
-}
-vec scale(double s, vec v)
-{
-    return {s * v[0], s * v[1], s * v[2]};
-}
 vec random_direction()
 {
     return normalized({ g(), g(), g() });
 }
-// Do gamma correction and truncation of color:
 int screen_color(double c)
 {
-    return min(int(pow(c * 255.0, 1.0 / 2.2)), 255);
+    return min(int(c * 255.0), 255);
+    //return min(int(pow(c * 255.0, 1.0 / 2.2)), 255);
 }
 
 vector<vec> sample_sphere(size_t num_points)
@@ -64,8 +56,7 @@ vector<vec> random_walk_on_sphere(size_t num_steps, double step_size)
     for (size_t i = 0; i < num_steps; ++i)
     {
         auto dir = random_direction();
-        auto offset = scale(step_size, dir);
-        point = normalized(add(point, offset));
+        point = normalized(point + step_size * dir);
         points.push_back(point);
     }
     return points;
@@ -90,14 +81,14 @@ vector<double> render_points(const vector<vec>& points)
     return image;
 }
 
-vec unfocus(vec v)
-{
-    const auto BLUR = 1.0;
-    const auto z = v[2];
-    const auto dir = random_direction();
-    const auto offset = scale(BLUR * z, dir);
-    return add(v, offset);
-}
+//vec unfocus(vec v)
+//{
+//    const auto BLUR = 1.0;
+//    const auto z = v[2];
+//    const auto dir = random_direction();
+//    const auto offset = scale(BLUR * z, dir);
+//    return add(v, offset);
+//}
 
 vector<double> render_line_segments(const vector<vec>& line_segments)
 {
@@ -106,16 +97,21 @@ vector<double> render_line_segments(const vector<vec>& line_segments)
     {
         const auto& p0 = line_segments[i + 0];
         const auto& p1 = line_segments[i + 1];
-        const auto NUM_SAMPLES = size_t{10};
+        const auto NUM_SAMPLES = size_t{30};
         for (size_t s = 0; s < NUM_SAMPLES; ++s)
         {
             const auto d = u();
-            const auto point = add(p0, scale(d, p1));
+            const auto point = (1.0 - d) * p0 + d * p1;
             // point = unfocus(point);
             const auto x = static_cast<size_t>(point[0] * RADIUS) + IMAGE_WIDTH  / 2;
             const auto y = static_cast<size_t>(point[1] * RADIUS) + IMAGE_HEIGHT / 2;
-            ++image[y * IMAGE_WIDTH + x];
+            image[y * IMAGE_WIDTH + x] = 1;
         }
+
+//        const auto x = static_cast<size_t>(p0[0] * RADIUS) + IMAGE_WIDTH  / 2;
+//        const auto y = static_cast<size_t>(p0[1] * RADIUS) + IMAGE_HEIGHT / 2;
+//        ++image[y * IMAGE_WIDTH + x];
+//        image[y * IMAGE_WIDTH + x] = 1.0;
     }
     const auto maximum = *max_element(begin(image), end(image));
     const auto normalization_factor = 1.0 / maximum;
@@ -142,8 +138,10 @@ vector<double> write_image(const vector<double>& image, const char* filename)
 
 int main()
 {
-    const auto points = sample_sphere(100);
-    cout << "Rendering camera image to file." << endl;
-    const auto image = render_points(points);
-    write_image(image, "cameraImage.ppm");
+    auto num_steps = 8000;
+    auto step_size = 0.09;
+    const auto points = random_walk_on_sphere(num_steps, step_size);
+    const auto image = render_line_segments(points);
+    write_image(image, "image.ppm");
+    return 0;
 }
