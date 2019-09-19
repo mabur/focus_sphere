d5c1bd325c3c5c016e07353cb5541b504223e60d
mabur <magnus.burenius@volumental.com>
2019-09-19 18:09:25 +0200

    Fix. Use explicit types to avoid crash in release only, possibly due to valarray

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <array>
+#include <chrono>
 #include <functional>
 #include <fstream>
 #include <iostream>
@@ -24,18 +25,31 @@ vec normalized(vec a)
 }
 
 const size_t NUM_COLOR_CHANNELS = 3;
-const size_t IMAGE_WIDTH  = 1000;
-const size_t IMAGE_HEIGHT = 1000;
+const size_t IMAGE_WIDTH  = 500;
+const size_t IMAGE_HEIGHT = 500;
+const double IMAGE_WIDTH_D  = IMAGE_WIDTH;
+const double IMAGE_HEIGHT_D = IMAGE_HEIGHT;
 const vec CAMERA_CENTER = {IMAGE_WIDTH  * 0.5, IMAGE_HEIGHT * 0.5, 0.0};
 const double RADIUS = min(IMAGE_HEIGHT, IMAGE_WIDTH) * 0.4;
 const size_t NUM_STEPS = 8000;
 const double STEP_SIZE = 0.09;
 const size_t NUM_SAMPLES_PER_LINE = 100;
 const double BRIGHTNESS = 1000.0;
-const auto BLUR_SCALING = {0.04};
-const auto BLUR_EXPONENT = {1.0};
+const auto BLUR_SCALING = 0.04;
+const auto NUM_FRAMES = 1;
+const auto ABERRATION = array<double,3>{1-.020, 1-.010, 1};
 
-vector<vec> random_walk_on_sphere(double blur_scaling, double blur_exponent)
+void rotate(vec& v, double a)
+{
+    const auto c = cos(a);
+    const auto s = sin(a);
+    const auto x = +c * v[0] + s * v[2];
+    const auto z = -s * v[0] + c * v[2];
+    v[0] = x;
+    v[2] = z;
+}
+
+vector<vec> random_walk_on_sphere(double blur_scaling, double angle)
 {
     auto g = bind(      normal_distribution<double>(), mt19937(0));
     auto u = bind(uniform_real_distribution<double>(), mt19937(0));
@@ -46,7 +60,7 @@ vector<vec> random_walk_on_sphere(double blur_scaling, double blur_exponent)
     };
 
     auto line_segments = vector<vec>{};
-    auto point = random_direction();
+    vec point = random_direction();
     for (size_t i = 0; i < NUM_STEPS; ++i)
     {
         auto dir = random_direction();
@@ -57,39 +71,37 @@ vector<vec> random_walk_on_sphere(double blur_scaling, double blur_exponent)
     auto image = vector<vec>(IMAGE_WIDTH * IMAGE_HEIGHT, vec{0, 0, 0});
     for (size_t i = 0; i < line_segments.size() - 1; ++i)
     {
-        const auto& p0 = line_segments[i + 0];
-        const auto& p1 = line_segments[i + 1];
+        const vec& p0 = line_segments[i + 0];
+        const vec& p1 = line_segments[i + 1];
         for (size_t s = 0; s < NUM_SAMPLES_PER_LINE; ++s)
         {
             const auto d = u();
-            auto point_world = vec{(1.0 - d) * p0 + d * p1};
+            vec point_world = vec{(1.0 - d) * p0 + d * p1};
+            rotate(point_world, angle);
+            const double focus_depth = 1.0;
+            const double dz = focus_depth - point_world[2];
+            point_world += blur_scaling * dz * random_direction();
 
-            const auto focus_depth = 1.0;
-            const auto dz = focus_depth - point_world[2];
-            point_world += blur_scaling * pow(dz, blur_exponent) * random_direction();
-
-            const auto ABERRATION = array<double,3>{1-.020, 1-.010, 1};
-
-            for (auto c = 0; c < NUM_COLOR_CHANNELS; ++c)
+            for (size_t c = 0; c < NUM_COLOR_CHANNELS; ++c)
             {
-                const auto point_aberrated = ABERRATION[c] * point_world;
-                const auto point_camera = RADIUS * point_aberrated + CAMERA_CENTER;
-                const auto x = point_camera[0];
-                const auto y = point_camera[1];
+                const vec point_aberrated = ABERRATION[c] * point_world;
+                const vec point_camera = RADIUS * point_aberrated + CAMERA_CENTER;
+                const double x = point_camera[0];
+                const double y = point_camera[1];
 
-                if (0.0 <= x && x < IMAGE_WIDTH && 0.0 <= y and y < IMAGE_HEIGHT)
+                if (0.0 <= x && x < IMAGE_WIDTH_D && 0.0 <= y && y < IMAGE_HEIGHT_D)
                 {
-                    const auto xi = static_cast<size_t>(x);
-                    const auto yi = static_cast<size_t>(y);
+                    const size_t xi = static_cast<size_t>(x);
+                    const size_t yi = static_cast<size_t>(y);
                     ++image[yi * IMAGE_WIDTH + xi][c];
                 }
             }
         }
     }
     const auto compare_pixels = [](vec a, vec b) {return a[0] < b[0];};
-    const auto maximum = *max_element(begin(image), end(image), compare_pixels);
-    const auto normalization_factor = 1.0 / maximum[0];
-    for (auto& pixel : image)
+    const vec maximum = *max_element(begin(image), end(image), compare_pixels);
+    const double normalization_factor = 1.0 / maximum[0];
+    for (vec& pixel : image)
     {
         pixel *= normalization_factor;
     }
@@ -100,9 +112,9 @@ void write_image(const vector<vec>& image, const char* filename)
 {
     ofstream file(filename);
     file << "P3" << endl << IMAGE_WIDTH << " " << IMAGE_HEIGHT << endl << "255" << endl;
-    for (const auto pixel : image)
+    for (const vec& pixel : image)
     {
-        for (auto c = 0; c < NUM_COLOR_CHANNELS; ++c)
+        for (size_t c = 0; c < NUM_COLOR_CHANNELS; ++c)
         {
             file << min(int(pixel[c] * BRIGHTNESS), 255) << " ";
         }
@@ -112,15 +124,18 @@ void write_image(const vector<vec>& image, const char* filename)
 
 int main()
 {
-    for (const auto blur_exponent : BLUR_EXPONENT)
+    for (size_t t = 0; t < NUM_FRAMES; ++t)
     {
-        for (const auto blur_scaling : BLUR_SCALING)
-        {
-            cout << "Exponent: " << blur_exponent << ". Scaling: " << blur_scaling << endl;
-            const auto image = random_walk_on_sphere(blur_scaling, blur_exponent);
-            const auto file_name = "image_" + to_string(blur_scaling) + "_" + to_string(blur_exponent) + ".ppm";
-            write_image(image, file_name.c_str());
-        }
+        const auto angle = double(t) / NUM_FRAMES * 2.0 * 3.14159265;
+
+        const auto start = chrono::system_clock::now();
+        cout << "Frame: " << t;
+        const auto image = random_walk_on_sphere(BLUR_SCALING, angle);
+        const auto file_name = "image_" + to_string(t) + ".ppm";
+        write_image(image, file_name.c_str());
+        const auto end = chrono::system_clock::now();
+        const auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+        cout << ". Took: "  << duration << endl;
     }
     return 0;
 }
